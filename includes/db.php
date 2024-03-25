53ae083c5b87777bbd3dce985831f99018c08eed
Emilie Valentin <valentin.emilie78@gmail.com>
2024-03-25 13:40:25 +0100

    index.php

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@
             $servername="db"; // Nom du serveur
             $username="root"; // Utilisateur
             $password="root"; // Mot de passe
-            $dbname="login"; // Nom de la base de données
+            $dbname="library"; // Nom de la base de données
 
             try{
                 $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password); // Effectue la connexion à la base de données
