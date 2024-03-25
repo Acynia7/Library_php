05bfc4761b231d7e416113281fd7b1a1038f13da
Emilie Valentin <valentin.emilie78@gmail.com>
2024-03-25 14:42:15 +0100

    CSS

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,18 @@
 <?php
     function connect(){
-            $servername="db"; // Nom du serveur
-            $username="root"; // Utilisateur
-            $password="root"; // Mot de passe
-            $dbname="library"; // Nom de la base de données
-
-            try{
-                $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password); // Effectue la connexion à la base de données
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $conn; // Retourne la connexion
-            }
-            catch (PDOException $e){
-                return null;
-                echo "Connexion à la base de donnée ratée!"; // Retourne null en cas d'échec de connexion
-            }
+        $servername = "db"; // Nom du serveur
+        $username = "root"; // Utilisateur
+        $password = "root"; // Mot de passe
+        $dbname = "library"; // Nom de la base de données
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password); // Effectue la connexion à la base de données
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connexion réussie!"; // Affiche un message en cas de succès de la connexion
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connexion à la base de données ratée: " . $e->getMessage(); // Affiche un message d'erreur en cas d'échec de connexion
+            return null;
+        }
     }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
     include ('includes/db.php');
     $conn = connect();
-
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Index </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/styles.css">
+    </head>
+
+    <body>
+        <div class="navbar"> Se connecter en tant qu'admin </div>
+    </body>
+    
+</html>
