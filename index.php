8c5aec3664421593b4e5ada387c37def92acce2a
Emilie Valentin <valentin.emilie78@gmail.com>
2024-03-27 11:59:19 +0100

    navbar

--- index.php
+++ index.php
@@ -8,12 +8,17 @@
         <title> Index </title>
         <meta charset="utf-8">
         <link rel="stylesheet" href="assets\style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     </head>
 
     <body>
-        <div class="navbar">
-        <a href="login.php">Se connecter en tant qu'admin</a>
-    </div>
+        <div class="container-fluid">
+            <div class="row">
+                    <div class="col col-2"> &nbsp; </div>
+                    <div class="col col-8 text-center bg-secondary-subtle text-right"> <a href="login.php">Connect as an admin</a> </div>
+                    <div class="col col-2"> &nbsp; </div>
+            </div>
+        </div>
     </body>
     
 </html>
